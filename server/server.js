6a521a71c82085a102adb973fa803a06b3b7b725
Ilyass <129749723+Ilyas-exe@users.noreply.github.com>
2025-09-29 16:34:37 +0100

    feat(server): implement mongodb database connection

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import questionRoutes from './routes/questionRoutes.js';
+import connectDB from './config/db.js';
 
 // Load env vars
 dotenv.config();
 
+// Connect to database
+connectDB();
+
 const app = express();
+
 
 // Middleware
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
+
+app.use('/api/questions', questionRoutes); 
 
 // Simple Route for testing
 app.get('/', (req, res) => {
